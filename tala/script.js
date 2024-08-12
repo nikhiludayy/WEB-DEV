f0b0a12436e8b0d6993f29fbf749c50fbec5ba20
nikhiludayy <nikhiluday0823@gmail.com>
2024-08-12 08:34:34 +0530

    2 page animation

--- tala/script.js
+++ tala/script.js
@@ -26,3 +26,48 @@ menu.addEventListener("click", function () {
     }
 
 })
+
+var tl=gsap.timeline();
+tl.from("#page1 h1",{
+    y:60,
+    duration:0.6,
+    opacity:0
+    
+})
+tl.from("#page1 h2",{
+    y:60,
+    duration:0.6,
+    opacity:0,
+    delay: -0.1
+})
+tl.from("#page1 h3",{
+    y:60,
+    duration:0.6,
+    opacity:0,
+    delay: -0.09
+})
+
+gsap.to("#page2 img",{
+
+    scale:1,
+    scrollTrigger:{
+        trigger:"#page2 img",
+        scroller:"body",
+        markers: false,
+        start:"top 70%",
+        end:"top 0%",
+        scrub:2
+    }
+})
+
+gsap.from("#page2 h1",{
+    rotateX:"90deg",
+    scrollTrigger:{
+        trigger:"#h11",
+        scroller:"body",
+        markers:true,
+        start:"top 70%",
+        end:"top 40%",
+        scrub:2
+    }
+})
