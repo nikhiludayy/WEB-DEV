514fad9d6a76ed43101b9788ca1206adf8a70ef0
nikhiludayy <nikhiluday0823@gmail.com>
2024-08-28 17:40:44 +0530

    id

--- ContextAPI/src/context/context.jsx
+++ ContextAPI/src/context/context.jsx
@@ -2,11 +2,11 @@ import React, { createContext, useState } from "react";
 export const UserContext = createContext();
 const context = (props) => {
   const [users, setUsers] = useState([
-    {id:0,usernames:"john",city:"new York"},
-    {id:1,usernames:"doe",city:"Los Angeles"},
-    {id:2,usernames:"jane",city:"Chicago"},
-    {id:3,usernames:"mark",city:"San Francisco"},
-    {id:4,usernames:"bill",city:"Seattle"}
+    {id:21230,usernames:"john",city:"new York"},
+    {id:14123,usernames:"doe",city:"Los Angeles"},
+    {id:21241,usernames:"jane",city:"Chicago"},
+    {id:31242,usernames:"mark",city:"San Francisco"},
+    {id:41424,usernames:"bill",city:"Seattle"}
     
   ]);
   return <UserContext.Provider value={{users, setUsers}}>{props.children}</UserContext.Provider>;
